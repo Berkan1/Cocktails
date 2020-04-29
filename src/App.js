e86f771263c40f2a0e768231450f7ecbae83004b
Berkan1 <berkan.c@hotmail.com>
2020-04-29 22:17:13 +0100

    Abstracting cocktail component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Axios from 'axios';
 
-class App extends React.Component {
-  state = {
+class Cocktails extends React.Component {
+	state = {
     cocktails: [],
   };
 
   componentDidMount() {
-    Axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=c').then(res => {
+    Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${this.props.firstLetter}`).then(res => {
       console.log(res);
       this.setState({ cocktails: res.data.drinks });
     });
@@ -20,6 +20,16 @@ class App extends React.Component {
     	  <ul>
           {this.state.cocktails.map(cocktail => <li>{cocktail.strDrink}</li>)}
         </ul>
+    	</div>
+    );
+  }	
+}
+
+class App extends React.Component {
+  render() {
+  	return (
+    	<div>
+    	  <Cocktails firstLetter='a'/>
     	</div>
     );
   }	
