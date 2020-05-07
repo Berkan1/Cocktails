a799d6f2376be9a0f18e6d738bccd73ffc402317
Berkan1 <berkan.c@hotmail.com>
2020-05-07 23:20:02 +0100

    Adding AwesomeButton library

--- src/components/home/Random.js
+++ src/components/home/Random.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Axios from 'axios';
+import { withRouter } from 'react-router-dom';
+import { AwesomeButton } from 'react-awesome-button';
+import "react-awesome-button/dist/styles.css";
+import '../../../src/App.css';
 
-function getRandomCocktail() {
-	let drinkId = '';
-  Axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php').then(res => {
-		drinkId = res.data.drinks[0].idDrink;
-		window.location=`/${drinkId}`;
-	});
+class Random extends React.Component {
+	getRandomCocktail() {
+		let drinkId = '';
+  	Axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php').then(res => {
+			drinkId = res.data.drinks[0].idDrink;
+			this.props.history.push(`/${drinkId}`);
+		});
+	}
+	
+	render() {
+    return (
+			<AwesomeButton type="primary" onPress={() => this.getRandomCocktail() }>Surprise me!</AwesomeButton>
+    );
+  }
 }
 
-function Random() {
-  return (
-  	<button onClick={getRandomCocktail} className="btn btn-primary">Surprise me!</button>
-  );
-} 
-
-export default Random;
+export default withRouter(Random);
