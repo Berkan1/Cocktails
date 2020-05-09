b95ccb882c26e7aba96f0edb2a914c15f3fa1a53
Berkan1 <berkan.c@hotmail.com>
2020-05-10 00:11:59 +0100

    Structuring cocktail details page

--- src/components/cocktail/CocktailDetails.js
+++ src/components/cocktail/CocktailDetails.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Badge } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { AwesomeButton } from 'react-awesome-button';
 import Axios from 'axios';
 
 function CocktailDetails(props) {
-	const [cocktailInfo, setCocktailInfo] = useState({image: '', name: '', ingredients: [], instructions: ''});
+	const [cocktailInfo, setCocktailInfo] = useState({
+		image: '', 
+		name: '', 
+		ingredients: [],
+		instructions: '',
+		alcoholic: '',
+		glass: ''
+	});
 
 	useEffect(() => {
 		Axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${props.match.params.id}`).then(res => {
@@ -14,8 +21,16 @@ function CocktailDetails(props) {
 
 			for (let i = 1; i <= 15; i++) {
 				const ingredient = cocktailData[`strIngredient${i}`];
+				let measurement = cocktailData[`strMeasure${i}`];
+				if (measurement) {
+					measurement = ` (${measurement.substring(0, measurement.length - 1)}) `;
+				}
+				else {
+					measurement = '';
+				}
+
 				if (ingredient){
-					ingredientsList.push(cocktailData[`strIngredient${i}`]);
+					ingredientsList.push(cocktailData[`strIngredient${i}`] + measurement);
 				}
 				else {
 					break;
@@ -26,33 +41,62 @@ function CocktailDetails(props) {
 				image: cocktailData.strDrinkThumb,
 				name: cocktailData.strDrink, 
 				ingredients: ingredientsList,
-				instructions: cocktailData.strInstructions
-			})
+				instructions: cocktailData.strInstructions,
+				alcoholic: cocktailData.strAlcoholic,
+				glass: cocktailData.strGlass
+			});
+
+			window.scrollTo(0, 0);
 		});
 	}, [props.match.params.id]);
 		
   return (
   	<Container>
-			<br/>
-			<p>
-				<NavLink to='/'>
-					<AwesomeButton type="primary">
-						<i className="glyphicon glyphicon-arrow-left back-icon"/>Back to cocktails
-					</AwesomeButton>
-				</NavLink>
-			</p>
-			<img src={cocktailInfo.image} alt={cocktailInfo.strDrink} width="400"></img>
-			<p>
-				<strong>{cocktailInfo.name}</strong>
-			</p>
-			<ul>
-        {cocktailInfo.ingredients.map(ingredient => 
-          <li key={ingredient}>
-						{ingredient}
-					</li> 
-        )}
-      </ul>
-			<p>{cocktailInfo.instructions}</p>
+			<Row>
+				<Col>
+				  <br/>
+					<NavLink to='/'>
+						<AwesomeButton type="primary">
+							<i className="glyphicon glyphicon-arrow-left back-icon"/>Back to cocktails
+						</AwesomeButton>
+					</NavLink>
+				</Col>
+			</Row>
+			<Row>
+				<Col md={5}>
+					<img src={cocktailInfo.image} alt={cocktailInfo.strDrink} width="100%" className="cocktail-image"></img>
+				</Col>
+				<Col md={7}>
+					<Row>
+						<Col>
+							<h2>{cocktailInfo.name}</h2>
+							<Badge pill variant="danger">
+								{cocktailInfo.alcoholic}
+  						</Badge>
+							<Badge pill variant="primary">
+								<i className="glyphicon glyphicon-glass icon-space"/>{cocktailInfo.glass}
+  						</Badge>
+							<hr/>
+						</Col>
+					</Row>
+					<Row>
+						<Col md={5}>
+							<h4>Ingredients</h4>
+							<ul>
+								{cocktailInfo.ingredients.map(ingredient => 
+									<li key={ingredient}>
+										{ingredient} 
+									</li> 
+								)}
+							</ul>
+						</Col>
+						<Col md={7}>
+							<h4>Instructions</h4>
+							<p>{cocktailInfo.instructions}</p>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
   	</Container>
   );
 } 
