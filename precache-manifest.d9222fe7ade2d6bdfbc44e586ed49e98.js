8c332a67a21d31dcc803c9e036dbb4268b2dee44
Berkan1 <berkan.c@hotmail.com>
2020-05-10 03:14:41 +0100

    Updates

---
+++ precache-manifest.d9222fe7ade2d6bdfbc44e586ed49e98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7245e19f08d97c105f8691c4f4bf06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "891b09d943f04e0da8c9",
+    "url": "/static/css/2.82ec1a9c.chunk.css"
+  },
+  {
+    "revision": "ce7b9c8d82456ff8c764",
+    "url": "/static/css/main.a9137f8e.chunk.css"
+  },
+  {
+    "revision": "891b09d943f04e0da8c9",
+    "url": "/static/js/2.36bf46e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.36bf46e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7b9c8d82456ff8c764",
+    "url": "/static/js/main.7224d9e5.chunk.js"
+  },
+  {
+    "revision": "53332d471775fce0d569",
+    "url": "/static/js/runtime-main.61ce9e2e.js"
+  }
+]);
