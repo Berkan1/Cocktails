97f62f37d12c5e47cb1ddeb354dbca0acdfd5dc5
Berkan1 <berkan.c@hotmail.com>
2020-05-10 02:33:14 +0100

    Updates

---
+++ precache-manifest.fc56f57c93d4050f08b58f7aca91ea07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797c3c092ba5bca4f1c8c4d548b6aacd",
+    "url": "/Cocktails/index.html"
+  },
+  {
+    "revision": "891b09d943f04e0da8c9",
+    "url": "/Cocktails/static/css/2.82ec1a9c.chunk.css"
+  },
+  {
+    "revision": "e15d14b24263278e50b1",
+    "url": "/Cocktails/static/css/main.a9137f8e.chunk.css"
+  },
+  {
+    "revision": "891b09d943f04e0da8c9",
+    "url": "/Cocktails/static/js/2.36bf46e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Cocktails/static/js/2.36bf46e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15d14b24263278e50b1",
+    "url": "/Cocktails/static/js/main.973f57e5.chunk.js"
+  },
+  {
+    "revision": "f0adffc9c5baae969215",
+    "url": "/Cocktails/static/js/runtime-main.f0a4fbc0.js"
+  }
+]);
